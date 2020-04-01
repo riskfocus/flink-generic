25701d80993432c5a0c4cc2b137e5a1ec15d24b9
Pavel Khokhlov <pavel.khokhlov@riskfocus.com>
2020-04-01 14:30:04 +0300

    Added Tool which provides return back contextId from Redis as well

---
+++ flink-snapshot/src/main/java/com.riskfocus.flink/snapshot/redis/RedisSnapshotConverterUtils.java
@@ -0,0 +1,35 @@
+package com.riskfocus.flink.snapshot.redis;
+
+import com.google.common.annotations.Beta;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonProcessingException;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.Serializable;
+
+/**
+ * @author Khokhlov Pavel
+ */
+@Beta
+public class RedisSnapshotConverterUtils<T> implements Serializable {
+
+    private static final long serialVersionUID = 7666812295659032901L;
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final String DELIMITER = ":";
+
+    static {
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    public ContextHolder<T> convertTo(Class<T> destClass, byte[] bytes) throws JsonProcessingException {
+        String src = new String(bytes);
+        int delimiterIdx = src.indexOf(DELIMITER);
+        String contextIdStr = src.substring(0, delimiterIdx);
+        String data = src.substring(delimiterIdx + 1);
+        long contextId = Long.parseLong(contextIdStr);
+        T t = mapper.readValue(data, destClass);
+        return new ContextHolder<>(contextId, t);
+    }
+
+}
