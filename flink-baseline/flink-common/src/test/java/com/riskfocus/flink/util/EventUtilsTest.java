363ce98e803d63e866b177e939a09f7abf210cbe
Pavel Khokhlov <pavel.khokhlov@riskfocus.com>
2020-04-14 10:38:05 +0300

    Added EventUtils

---
+++ flink-baseline/flink-common/src/test/java/com/riskfocus/flink/util/EventUtilsTest.java
@@ -0,0 +1,40 @@
+package com.riskfocus.flink.util;
+
+import com.riskfocus.flink.domain.Event;
+import com.riskfocus.flink.domain.IncomingEvent;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Khokhlov Pavel
+ */
+public class EventUtilsTest {
+
+    @Test
+    public void checkUpdateRequired() {
+
+        Event previous = buildEvent(1);
+
+        Event current = buildEvent(2);
+
+        Assert.assertTrue(EventUtils.updateRequired(previous, current));
+
+        Event equalsTime = buildEvent(1);
+        Assert.assertTrue(EventUtils.updateRequired(previous, equalsTime));
+
+        Event biggerTime = buildEvent(3);
+        Assert.assertTrue(EventUtils.updateRequired(previous, biggerTime));
+
+        Event lessTime = buildEvent(0);
+        Assert.assertFalse(EventUtils.updateRequired(previous, lessTime));
+
+    }
+
+    private Event buildEvent(long timestamp) {
+        Event price = new IncomingEvent() {
+
+        };
+        price.setTimestamp(timestamp);
+        return price;
+    }
+}
