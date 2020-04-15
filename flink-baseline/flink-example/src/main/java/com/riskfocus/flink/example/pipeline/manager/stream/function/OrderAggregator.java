38cc3d37f0402413b9b4bcc42ddd61fe2a556ec8
Pavel Khokhlov <pavel.khokhlov@riskfocus.com>
2020-04-15 15:42:23 +0300

    Fixed NPE on application side

--- flink-baseline/flink-example/src/main/java/com/riskfocus/flink/example/pipeline/manager/stream/function/OrderAggregator.java
+++ flink-baseline/flink-example/src/main/java/com/riskfocus/flink/example/pipeline/manager/stream/function/OrderAggregator.java
@@ -1,8 +1,12 @@
 package com.riskfocus.flink.example.pipeline.manager.stream.function;
 
+import com.riskfocus.flink.example.pipeline.domain.Order;
 import com.riskfocus.flink.example.pipeline.domain.Purchase;
 import com.riskfocus.flink.example.pipeline.domain.intermediate.CustomerAndAccountAndOrder;
 import org.apache.flink.api.common.functions.AggregateFunction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Khokhlov Pavel
@@ -11,11 +15,13 @@ public class OrderAggregator implements AggregateFunction<CustomerAndAccountAndO
 
     @Override
     public Purchase createAccumulator() {
-        return Purchase.builder().build();
+        List<Order> orders = new ArrayList<>();
+        return Purchase.builder().orders(orders).build();
     }
 
     @Override
     public Purchase add(CustomerAndAccountAndOrder value, Purchase accumulator) {
+        accumulator.setCustomer(value.getCustomer());
         accumulator.setTimestamp(value.getTimestamp());
         accumulator.getOrders().add(value.getOrder());
         return accumulator;
