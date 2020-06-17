60f224ff568f751864749d90a09cad7112fdd1be
Pavel Khokhlov <pavel.hohlov@bsc-ideas.com>
2020-06-17 12:58:06 +0300

    Downgrade Flink to 1.10 version Switch to JDK 8 (needed for AWS EMR support)

--- flink-baseline/flink-snapshot/src/main/java/com/riskfocus/flink/snapshot/context/ContextServiceProvider.java
+++ flink-baseline/flink-snapshot/src/main/java/com/riskfocus/flink/snapshot/context/ContextServiceProvider.java
@@ -1,6 +1,5 @@
 package com.riskfocus.flink.snapshot.context;
 
-import com.riskfocus.flink.snapshot.context.rest.RestBased;
 import com.riskfocus.flink.util.ParamUtils;
 import com.riskfocus.flink.window.WindowAware;
 import com.riskfocus.flink.window.generator.GeneratorType;
@@ -15,7 +14,6 @@ import lombok.NoArgsConstructor;
 public final class ContextServiceProvider {
 
     public static final String CONTEXT_PROVIDER_TYPE_PARAM_NAME = "context.provider.type";
-    public static final String CONTEXT_SERVICE_URL_PARAM_NAME = "context.service.url";
 
     public static ContextService create(ParamUtils params) {
         String batchTypeStr = params.getString(CONTEXT_PROVIDER_TYPE_PARAM_NAME, GeneratorType.BASIC.name());
@@ -24,9 +22,6 @@ public final class ContextServiceProvider {
         switch (generatorType) {
             case BASIC:
                 return new WindowBased(windowAware);
-            case REST:
-                String url = params.getString(CONTEXT_SERVICE_URL_PARAM_NAME, "http://test.example.com");
-                return new RestBased(windowAware, url);
             default:
                 throw new UnsupportedOperationException("Implementation required");
         }
