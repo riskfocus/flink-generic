302fec2d0a2bccf16943edfa56bcd42df534bc60
Pavel Khokhlov <pavel.khokhlov@riskfocus.com>
2020-07-27 17:19:02 +0300

    ICD-125: Fix after Code review.
    Refactoring: isPrimary -> primaryKey
    Use generic SQL API to get table description
    Removed copy/paste with batch logic
    Removed unused function

--- flink-baseline/flink-jdbc-sink/src/main/java/com/riskfocus/flink/sink/jdbc/core/executor/SimpleBatchStatementExecutor.java
+++ flink-baseline/flink-jdbc-sink/src/main/java/com/riskfocus/flink/sink/jdbc/core/executor/SimpleBatchStatementExecutor.java
@@ -5,74 +5,35 @@
 package com.riskfocus.flink.sink.jdbc.core.executor;
 
 import com.riskfocus.flink.sink.jdbc.config.JdbcExecutionOptions;
+import com.riskfocus.flink.sink.jdbc.core.JdbcStatementBuilderWithMetadata;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.java.io.jdbc.JdbcStatementBuilder;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Function;
 
 /**
  * @author Khokhlov Pavel
  */
 @Slf4j
-public class SimpleBatchStatementExecutor<T, V> implements JdbcBatchStatementExecutor<T> {
+public class SimpleBatchStatementExecutor<T, V> extends AbstractSimpleBatchStatementExecutor<T, V> {
 
     private final String sql;
     private final JdbcStatementBuilder<V> parameterSetter;
-    private final Function<T, V> valueTransformer;
-    private final transient List<V> batch;
-
-    private transient PreparedStatement st;
-    private transient Connection connection;
 
     public SimpleBatchStatementExecutor(String sql, JdbcStatementBuilder<V> statementBuilder, Function<T, V> valueTransformer,
                                         JdbcExecutionOptions jdbcExecutionOptions) {
+        super(valueTransformer, jdbcExecutionOptions);
         this.sql = sql;
         this.parameterSetter = statementBuilder;
-        this.valueTransformer = valueTransformer;
-        this.batch = new ArrayList<>(jdbcExecutionOptions.getBatchSize());
     }
 
     @Override
-    public void open(Connection connection) throws SQLException {
-        this.connection = connection;
-        this.st = connection.prepareStatement(sql);
+    String getSQL() {
+        return sql;
     }
 
     @Override
-    public void addToBatch(T record) {
-        batch.add(valueTransformer.apply(record));
-    }
-
-    @Override
-    public void executeBatch() throws SQLException {
-        if (!batch.isEmpty()) {
-            for (V r : batch) {
-                parameterSetter.accept(st, r);
-                st.addBatch();
-                log.trace("Added to batch: {}", r);
-            }
-            st.executeBatch();
-            if (!connection.getAutoCommit()) {
-                log.trace("Commit batch: {}", batch.size());
-                connection.commit();
-            }
-            batch.clear();
-        }
-    }
-
-    @Override
-    public void close() throws SQLException {
-        if (st != null) {
-            st.close();
-            st = null;
-        }
-        if (batch != null) {
-            batch.clear();
-        }
+    JdbcStatementBuilderWithMetadata<V> getStatementConsumer() {
+        return (preparedStatement, fieldMetadata, v) -> parameterSetter.accept(preparedStatement, v);
     }
 }
