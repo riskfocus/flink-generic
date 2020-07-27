9158a62a56f36a6892cffbcd72b74b6aaf4139fb
Pavel Khokhlov <pavel.khokhlov@riskfocus.com>
2020-07-27 17:19:02 +0300

    ICD-125: Fix after Code review.
    Refactoring: isPrimary -> primaryKey
    Use generic SQL API to get table description
    Removed copy/paste with batch logic
    Removed unused function

--- flink-baseline/flink-jdbc-sink/src/main/java/com/riskfocus/flink/sink/jdbc/core/executor/WithTableMetadataBatchStatementExecutor.java
+++ flink-baseline/flink-jdbc-sink/src/main/java/com/riskfocus/flink/sink/jdbc/core/executor/WithTableMetadataBatchStatementExecutor.java
@@ -10,8 +10,12 @@ import com.riskfocus.flink.sink.jdbc.core.JdbcSqlBuilderWithMetadata;
 import com.riskfocus.flink.sink.jdbc.core.JdbcStatementBuilderWithMetadata;
 import lombok.extern.slf4j.Slf4j;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.JDBCType;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 
@@ -20,89 +24,81 @@ import java.util.function.Function;
  * @author NIakovlev
  */
 @Slf4j
-public class WithTableMetadataBatchStatementExecutor<T, V> implements JdbcBatchStatementExecutor<T> {
+public class WithTableMetadataBatchStatementExecutor<T, V> extends AbstractSimpleBatchStatementExecutor<T, V> {
 
     private final String tableName;
-    private final JdbcSqlBuilderWithMetadata sql;
+    private final JdbcSqlBuilderWithMetadata sqlBuilderWithMetadata;
     private final JdbcStatementBuilderWithMetadata<V> statementBuilder;
-    private final Function<T, V> valueTransformer;
-    private final transient List<V> batch;
 
-    private transient PreparedStatement st;
-    private transient Connection connection;
     private transient List<FieldMetadata> tableMetadata;
 
     public WithTableMetadataBatchStatementExecutor(String tableName,
-                                                   JdbcSqlBuilderWithMetadata sql,
+                                                   JdbcSqlBuilderWithMetadata sqlBuilderWithMetadata,
                                                    JdbcStatementBuilderWithMetadata<V> statementBuilder,
                                                    Function<T, V> valueTransformer,
                                                    JdbcExecutionOptions jdbcExecutionOptions) {
+        super(valueTransformer, jdbcExecutionOptions);
         this.tableName = tableName;
-        this.sql = sql;
+        this.sqlBuilderWithMetadata = sqlBuilderWithMetadata;
         this.statementBuilder = statementBuilder;
-        this.valueTransformer = valueTransformer;
-        this.batch = new ArrayList<>(jdbcExecutionOptions.getBatchSize());
     }
 
     @Override
-    public void open(Connection connection) throws SQLException {
-        this.connection = connection;
+    void init(Connection connection) throws SQLException {
         this.tableMetadata = getTableMetadata();
-        this.st = connection.prepareStatement(sql.apply(tableMetadata));
+    }
+
+    @Override
+    JdbcStatementBuilderWithMetadata<V> getStatementConsumer() {
+        return statementBuilder;
+    }
+
+    @Override
+    String getSQL() {
+        return sqlBuilderWithMetadata.apply(tableMetadata);
+    }
+
+    @Override
+    Collection<FieldMetadata> getMetadata() {
+        return tableMetadata;
     }
 
     private List<FieldMetadata> getTableMetadata() throws SQLException {
-        ResultSet descSet = connection.prepareStatement("DESCRIBE " + tableName).executeQuery();
-        ResultSet columnsSet = connection.prepareStatement("select * from " + tableName + " limit 0").executeQuery();
-
-        ResultSetMetaData metaData = columnsSet.getMetaData();
         List<FieldMetadata> fieldMetadataList = new ArrayList<>();
-        for (int i = 1; i <= metaData.getColumnCount(); i++) {
-            descSet.next(); //move the cursor
+        List<String> primaryKeys = getPrimaryKeysDesc();
 
-            fieldMetadataList.add(FieldMetadata.builder()
-                    .fieldName(metaData.getColumnName(i))
-                    .jdbcType(JDBCType.valueOf(metaData.getColumnType(i)))
-                    .nullable("YES".equals(descSet.getString(3)))
-                    .isPrimary("PRI".equals(descSet.getString(4)))
-                    .withDefault(descSet.getObject(5) != null)
-                    .build()
-            );
+        try (ResultSet columns = connection.getMetaData().getColumns(null, null, tableName, null)) {
+            while (columns.next()) {
+                // See java.sql.DatabaseMetaData.getColumns documentation
+                String columnName = columns.getString("COLUMN_NAME");
+                JDBCType dataType = JDBCType.valueOf(columns.getInt("DATA_TYPE"));
+                String nullabe = columns.getString("IS_NULLABLE");
+                String defaultValue = columns.getString("COLUMN_DEF");
+                boolean primaryKey = primaryKeys.contains(columnName);
+                log.info("Got description for table: {}, columnName: {}, dataType: {}, nullabe: {}, defaultValue: {}, primaryKey: {}", tableName, columnName,
+                        dataType, nullabe, defaultValue,
+                        primaryKey);
+                fieldMetadataList.add(FieldMetadata.builder()
+                        .fieldName(columnName)
+                        .jdbcType(dataType)
+                        .nullable("YES".equalsIgnoreCase(nullabe))
+                        .primaryKey(primaryKey)
+                        .withDefault(defaultValue != null)
+                        .build());
+            }
         }
-        connection.commit(); //close transaction for releasing metadata lock
         return fieldMetadataList;
     }
 
-    @Override
-    public void addToBatch(T record) {
-        batch.add(valueTransformer.apply(record));
+    private List<String> getPrimaryKeysDesc() throws SQLException {
+        List<String> res = new ArrayList<>();
+        try (ResultSet primaryKeys = connection.getMetaData().getPrimaryKeys(null, null, tableName)) {
+            while (primaryKeys.next()) {
+                String column = primaryKeys.getString("COLUMN_NAME");
+                res.add(column);
+            }
+        }
+        return res;
     }
 
-    @Override
-    public void executeBatch() throws SQLException {
-        if (!batch.isEmpty()) {
-            for (V r : batch) {
-                statementBuilder.accept(st, tableMetadata, r);
-                st.addBatch();
-                log.trace("Added to batch: {}", r);
-            }
-            st.executeBatch();
-            if (!connection.getAutoCommit()) {
-                log.trace("Commit batch: {}", batch.size());
-                connection.commit();
-            }
-            batch.clear();
-        }
-    }
-
-    @Override
-    public void close() throws SQLException {
-        if (st != null) {
-            st.close();
-            st = null;
-        }
-        if (batch != null) {
-            batch.clear();
-        }
-    }
 }
