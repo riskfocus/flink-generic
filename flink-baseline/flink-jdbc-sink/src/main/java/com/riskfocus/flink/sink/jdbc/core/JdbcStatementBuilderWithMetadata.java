372c8f5c7bc6d32a07766574788204c254b23c78
Pavel Khokhlov <pavel.khokhlov@riskfocus.com>
2020-07-27 17:19:02 +0300

    ICD-125: Fix after Code review.
    Refactoring: isPrimary -> primaryKey
    Use generic SQL API to get table description
    Removed copy/paste with batch logic
    Removed unused function

--- flink-baseline/flink-jdbc-sink/src/main/java/com/riskfocus/flink/sink/jdbc/core/JdbcStatementBuilderWithMetadata.java
+++ flink-baseline/flink-jdbc-sink/src/main/java/com/riskfocus/flink/sink/jdbc/core/JdbcStatementBuilderWithMetadata.java
@@ -9,17 +9,17 @@ import org.apache.flink.annotation.PublicEvolving;
 import java.io.Serializable;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.Collection;
 
 /**
  * Consumer function with 3 arguments:
  * <ul>
  *     <li>{@link PreparedStatement} - statement to be filled in with new values</li>
- *     <li>List<{@link FieldMetadata}> - table metadata with list of column info</li>
+ *     <li>Collection<{@link FieldMetadata}> - table metadata with list of column info</li>
  *     <li>T - new record to be processed by the Sink function</li>
  * </ul>
  * @param <T>
  */
 @PublicEvolving
-public interface JdbcStatementBuilderWithMetadata<T> extends TriConsumerWithException<PreparedStatement, List<FieldMetadata>, T, SQLException>, Serializable {
+public interface JdbcStatementBuilderWithMetadata<T> extends TriConsumerWithException<PreparedStatement, Collection<FieldMetadata>, T, SQLException>, Serializable {
 }
