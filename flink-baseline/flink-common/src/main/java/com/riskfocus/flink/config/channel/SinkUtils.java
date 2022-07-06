eac1118324d7c0c306a0dd69fa4c04275d74a11f
Jim Zucker <jim@zucker.us>
2022-07-06 15:41:08 -0700

    Updated header

--- flink-baseline/flink-common/src/main/java/com/riskfocus/flink/config/channel/SinkUtils.java
+++ flink-baseline/flink-common/src/main/java/com/riskfocus/flink/config/channel/SinkUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Risk Focus Inc
+ * Copyright 2020-2022 Ness USA, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
